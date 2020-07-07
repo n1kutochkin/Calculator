caffb4cda119326d0d30b5576998e0379d472db3
Nikita Utochkin <nikutochkin28@gmail.com>
2020-07-07 18:22:46 +0300

    added main

---
+++ src/ru/n1kutochkin/Main.java
@@ -0,0 +1,14 @@
+package ru.n1kutochkin;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Parser parser = new Parser(in.nextLine());
+
+
+    }
+}
