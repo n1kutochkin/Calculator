1ed6b12f7f39acd43d517831fdb7e1c388878d23
Nikita Utochkin <nikutochkin28@gmail.com>
2020-07-08 22:30:34 +0300

    workable for arabic numbers

--- src/ru/n1kutochkin/Main.java
+++ src/ru/n1kutochkin/Main.java
@@ -6,9 +6,13 @@ public class Main {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Parser parser = new Parser(in.nextLine());
 
-
+        if (parser.getResult().isPresent()) {
+           System.out.println(parser.getResult().get().getResult());
+        } else {
+            throw new Exception();
+        }
     }
 }
